14a16e4d449cb5a1b9a818843e703e4d5ea6d7ea
viacheslav liakh <slavon360@gmail.com>
2019-07-18 00:17:09 +0300

    telegram bot begin

--- routes/views/makeOrder.js
+++ routes/views/makeOrder.js
@@ -1,5 +1,36 @@
 var keystone = require('keystone');
 var ClientOrder = keystone.list('ClientOrder');
+const axios = require('axios');
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '949146703:AAEX0hDtWofS-FMnQ-AOsuA1BhWX3ltajgw';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   // 'msg' is the received Message from Telegram
+//   // 'match' is the result of executing the regexp above on the text content
+//   // of the message
+
+//   const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
+
+//   // send back the matched "whatever" to the chat
+//   bot.sendMessage(chatId, resp);
+// });
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//   console.log(msg);
+
+//   // send a message to the chat acknowledging receipt of their message
+//   bot.sendMessage(251733133, 'Received your message');
+// });
 
 var sendJSONresponse = function(res,status,content){
 	res.json(content);
@@ -20,6 +51,7 @@ exports=module.exports=function(req,res, next){
 			sendJSONresponse(res, 500, { error: 'Internal Server Error' });
 			throw err;
 		};
-		sendJSONresponse(res,200,order);		
+		sendJSONresponse(res,200,order);
+		bot.sendMessage(251733133, JSON.stringify(order));
 	})
 }
