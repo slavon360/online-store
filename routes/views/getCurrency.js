670c003ebc33d5e9a1dcf746b62dcb1321c7d0ac
viacheslav liakh <slavon360@gmail.com>
2019-09-12 23:55:48 +0300

    getCUrrency test

--- routes/views/getCurrency.js
+++ routes/views/getCurrency.js
@@ -1,5 +1,8 @@
 var keystone = require('keystone');
 var searched_id = process.env.CURRENCY_ID;
+const axios = require('axios');
+
+const token = '949146703:AAEX0hDtWofS-FMnQ-AOsuA1BhWX3ltajgw';
 
 module.exports = function(req, res){
     keystone.list('Currency')
@@ -7,8 +10,22 @@ module.exports = function(req, res){
     .findById(searched_id)
     .exec(function(err, currency){
         try {
+            axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=251733133&text=currency:${currency}&parse_mode=HTML`)
+                .then((res) => {
+                    
+                })
+                .catch((error) => {
+                    
+                })
             res.send(currency);
         } catch {
+            axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=251733133&text=error:${err}&parse_mode=HTML`)
+                .then((res) => {
+                    
+                })
+                .catch((error) => {
+                    
+                })
             console.log(err)
             res.send(err);
         }
