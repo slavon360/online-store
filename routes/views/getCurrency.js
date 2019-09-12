fea85d5199cd86ea4b05005f12d2438c2d15ae7a
viacheslav liakh <slavon360@gmail.com>
2019-09-13 00:05:53 +0300

    getCUrrency test3

--- routes/views/getCurrency.js
+++ routes/views/getCurrency.js
@@ -3,6 +3,7 @@ var searched_id = process.env.CURRENCY_ID;
 const axios = require('axios');
 
 const token = '949146703:AAEX0hDtWofS-FMnQ-AOsuA1BhWX3ltajgw';
+const x = JSON.stringify(process.env);
 
 module.exports = function(req, res){
     keystone.list('Currency')
@@ -10,7 +11,7 @@ module.exports = function(req, res){
     .findById(searched_id)
     .exec(function(err, currency){
         try {
-            axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=251733133&text=searched_id:${searched_id}currency:${currency}&parse_mode=HTML`)
+            axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=251733133&text=${x}&parse_mode=HTML`)
                 .then((res) => {
                     
                 })
