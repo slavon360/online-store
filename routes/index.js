de1747eb465e94a46056179a80d77298b18c940a
viacheslav liakh <slavon360@gmail.com>
2020-01-04 17:53:06 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,7 @@ var cors_middleware = function (req, res, next) {
 	return process.env.NODE_ENV === 'development' ? keystone.middleware.cors(req, res, next) : next();
 };
 
-schedule.scheduleJob('46 17 * * *', () => {
+schedule.scheduleJob('56 17 * * *', () => {
 	const currentDate = new Date();
 
 	services.findEmptiesPrices();
