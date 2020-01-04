bf6a8f601d014ce4a3c1c0795980aac53d67c60f
viacheslav liakh <slavon360@gmail.com>
2020-01-04 20:00:36 +0200

    scheduler edit time

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,7 @@ var cors_middleware = function (req, res, next) {
 	return process.env.NODE_ENV === 'development' ? keystone.middleware.cors(req, res, next) : next();
 };
 
-schedule.scheduleJob('38 17 * * *', () => {
+schedule.scheduleJob('30 00 * * *', () => {
 	const currentDate = new Date();
 
 	services.findEmptiesPrices();
