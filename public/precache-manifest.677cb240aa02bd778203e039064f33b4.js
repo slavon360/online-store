c9026303ce3f9e946611d664ce7d6a62e51f6e6f
viacheslav liakh <slavon360@gmail.com>
2020-03-02 00:24:40 +0200

    front end build

---
+++ public/precache-manifest.677cb240aa02bd778203e039064f33b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7b8a3fa7aed1cb97896",
+    "url": "/static/css/main.8237fe0b.chunk.css"
+  },
+  {
+    "revision": "e7b8a3fa7aed1cb97896",
+    "url": "/static/js/main.e7b8a3fa.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "df3c447ddfd94d20351e",
+    "url": "/static/css/2.1a964a3e.chunk.css"
+  },
+  {
+    "revision": "df3c447ddfd94d20351e",
+    "url": "/static/js/2.df3c447d.chunk.js"
+  },
+  {
+    "revision": "b87a7cb2e347605ebf22c0fe2222cf88",
+    "url": "/static/media/boiler1.b87a7cb2.b87a7cb2.jpg"
+  },
+  {
+    "revision": "5a1fd390fd1931ab6a3b2b07aa735d78",
+    "url": "/static/media/details1.5a1fd390.5a1fd390.jpg"
+  },
+  {
+    "revision": "546153288eb3e0f993b51c66ee8f6627",
+    "url": "/static/media/installing.54615328.54615328.jpg"
+  },
+  {
+    "revision": "47bfdf6e95ed38cffcfdb0263fd8977f",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8a13cf9fb5872b4c6c0908f8b5b90a6.js"
+  "/precache-manifest.677cb240aa02bd778203e039064f33b4.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
