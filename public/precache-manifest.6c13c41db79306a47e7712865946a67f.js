26843be66f89c938ad1c33eda030891a4144d249
slavon360 <slavon360@github.com>
2021-03-28 19:53:51 +0300

    icons added

--- public/precache-manifest.6c13c41db79306a47e7712865946a67f.js
+++ public/precache-manifest.6c13c41db79306a47e7712865946a67f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "0026164dd746048311d9",
-    "url": "/static/css/main.f342e004.chunk.css"
+    "revision": "fd3527e541adf40c56f0",
+    "url": "/static/css/main.86ff3a8c.chunk.css"
   },
   {
-    "revision": "0026164dd746048311d9",
-    "url": "/static/js/main.0026164d.chunk.js"
+    "revision": "fd3527e541adf40c56f0",
+    "url": "/static/js/main.fd3527e5.chunk.js"
   },
   {
     "revision": "1b922744246c39b6bc43",
     "url": "/static/js/runtime~main.1b922744.js"
   },
   {
-    "revision": "d621102c9b2957384511",
+    "revision": "5fea80924d7052056acd",
     "url": "/static/css/2.4c3b131d.chunk.css"
   },
   {
-    "revision": "d621102c9b2957384511",
-    "url": "/static/js/2.d621102c.chunk.js"
+    "revision": "5fea80924d7052056acd",
+    "url": "/static/js/2.5fea8092.chunk.js"
   },
   {
     "revision": "3469a1f8757c158c75f7dc6b7933ce7c",
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/static/media/banner-repair.d42be307.jpg"
   },
   {
-    "revision": "4aa34eb8c6da973fbc3a65502c6783c6",
+    "revision": "0990ca0561662cf42db39f04dd77b856",
     "url": "/index.html"
   }
 ];
